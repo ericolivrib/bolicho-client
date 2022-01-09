2678edd161e698f1cb7f89abfc8a94d7c8db4ea4
ericolivrib <ribericoliveira@protonmail.ch>
2022-01-08 23:28:38 -0300

    Pasta utils mudou de diretório

--- src/app/components/principal/principal.module.ts
+++ src/app/components/principal/principal.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { PrincipalComponent } from './principal.component';
 import { PrincipalRoutingModule } from './principal-routing.module';
-import { MenuLateralComponent } from '../utils/menu-lateral/menu-lateral.component';
-import { MenuSuperiorComponent } from '../utils/menu-superior/menu-superior.component';
+import { MenuLateralComponent } from '../../utils/menu-lateral/menu-lateral.component';
+import { MenuSuperiorComponent } from '../../utils/menu-superior/menu-superior.component';
 
 @NgModule({
    declarations: [
@@ -15,6 +15,6 @@ import { MenuSuperiorComponent } from '../utils/menu-superior/menu-superior.comp
    imports: [
       CommonModule,
       PrincipalRoutingModule
-   ],
+   ]
 })
 export class PrincipalModule {}
