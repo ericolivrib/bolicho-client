76a894484a75b878d9df6351ea88cb2d4861f3f9
ericolivrib <ribericoliveira@protonmail.ch>
2022-01-13 17:29:07 -0300

    componente renomeado de NaoEncontrado para NotFound

---
+++ src/app/components/not-found/not-found.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nao-encontrado',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.css']
+})
+export class NotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
