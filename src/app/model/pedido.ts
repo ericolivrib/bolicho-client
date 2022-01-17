67beb051fdb9b427ad546c0e8412e20bf393c955
ericolivrib <ribericoliveira@protonmail.ch>
2022-01-17 15:33:27 -0300

    tipo de variável numPedido alterado para string

--- src/app/model/pedido.ts
+++ src/app/model/pedido.ts
@@ -5,7 +5,7 @@ import { Endereco } from "./endereco";
 export class Pedido {
 
    public id: number;
-   public numPedido: number;
+   public numPedido: string;
    public cliente: Cliente;
    public itens: Item[];
    public enderecoEntrega: Endereco;
@@ -15,9 +15,9 @@ export class Pedido {
    public valorTotal: number;
    public status: string;
 
-   constructor(id?: number, numPedido?: number, cliente?: Cliente, itens?: Item[], enderecoEntrega?: Endereco, dataPedido?: Date, dataEntrega?: Date, dataFinalizado?: Date, valorTotal?: number, status?: string) {
+   constructor(id?: number, numPedido?: string, cliente?: Cliente, itens?: Item[], enderecoEntrega?: Endereco, dataPedido?: Date, dataEntrega?: Date, dataFinalizado?: Date, valorTotal?: number, status?: string) {
       this.id = <number>id;
-      this.numPedido = <number>numPedido;
+      this.numPedido = <string>numPedido;
       this.cliente = <Cliente>cliente;
       this.itens = <Item[]>itens;
       this.enderecoEntrega = <Endereco>enderecoEntrega;
